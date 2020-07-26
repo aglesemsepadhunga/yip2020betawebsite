630ad1889ea9c3ebb3c4152ddc13882c9c10afcb
“aglesemsepadhunga” <shivoy4ndixit@gmail.com>
2020-07-26 23:16:52 +0530

    now we can add images

--- yip-project/yip/settings.py
+++ yip-project/yip/settings.py
@@ -118,4 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'yip/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+# addres of uploaded media including pics
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
